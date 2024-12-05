21e904b794af633394bf83c477e90f62caeeda91
Csóti István <109945490+csotiistvan@users.noreply.github.com>
2024-12-05 11:50:28 +0100

    Adding checks to fingerprints.sh to prevent whitelisting empty certs

--- fingerprints.sh
+++ fingerprints.sh
@@ -8,8 +8,12 @@ echo "default 1;" >> $nginx_conf
 for cert in $(/usr/bin/find $1 -path **/TLS/* -name TLS*.pem)
 do
     fingerprint=$(openssl x509 -in "$cert" -noout -fingerprint -sha1 | sed 's/SHA1 Fingerprint=//;s/sha1 Fingerprint=//; s/://g')
-    echo "$fingerprint 0;" >> $nginx_conf
+    # Check to prevent whitelisting empty certs
+    if [ "$(echo -n "$fingerprint" | wc -m)" -eq 40 ]; then
+        echo "$fingerprint 0;" >> "$nginx_conf"
+    else
+        echo "##Invalid fingerprint: $cert" >> "$nginx_conf"
+    fi
 done
 
 echo "}" >> $nginx_conf
-
