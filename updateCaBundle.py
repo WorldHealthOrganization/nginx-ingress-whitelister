3a10dc7e94dae170619123097ffaf73b6cce6014
Csóti István <109945490+csotiistvan@users.noreply.github.com>
2025-02-06 14:31:07 +0100

    Revert updateCaBundle.py

--- updateCaBundle.py
+++ updateCaBundle.py
@@ -1,38 +1,18 @@
 import os
 import glob
 import base64
-import re
 from kubernetes import client, config
 
 config.load_incluster_config()
 
 bundle_namespace = os.environ.get("BUNDLE_NAMESPACE")
-bundle_name = os.environ.get("BUNDLE_NAME")
-
-# Cert validation function
-def is_valid_pem_cert(content):
-  pem_pattern = re.compile(
-      r"-----BEGIN CERTIFICATE-----\s+([a-zA-Z0-9+/=\s]+)\s+-----END CERTIFICATE-----",
-      re.DOTALL
-  )
-  match = pem_pattern.search(content)
-  if not match:
-      return False
-  try:
-      base64.b64decode(match.group(1), validate=True)
-      return True
-  except base64.binascii.Error:
-      return False
-
+bundle_name=os.environ.get("BUNDLE_NAME")
 files = glob.glob("./certificateFolder/**/TLS/CA*.pem", recursive=True)
 ca_bundle = ""
 for file in files:
   with open(file) as f:
     data = f.read()
-  if is_valid_pem_cert(data):
-      ca_bundle += "\n" + data
-  else:
-      print(f"Skipping invalid certificate file: {file}")
+  ca_bundle = ca_bundle+"\n"+data
   
 if len(files) and ca_bundle:
   api_instance = client.CoreV1Api()
